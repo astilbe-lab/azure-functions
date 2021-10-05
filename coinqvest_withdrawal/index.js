1493179f3e053c7445ec58c3a4384afac6e66883
Dawit Amanuel <ahizkias@gmail.com>
2021-10-05 16:16:31 +0300

    [improve] Add support for all asset requests. No validation yet

--- coinqvest_withdrawal/index.js
+++ coinqvest_withdrawal/index.js
@@ -11,24 +11,20 @@ module.exports = async function (context, req) {
         const amount = req.body.amount;
         const targetNetwork = req.body.targetNetwork;
         const targetAsset = req.body.targetAsset;
-        const memo = req.body.memo;
-        const krackenWalletId = req.body.krackenWalletId;
         const sourceAsset = req.body.sourceAsset;
+        const relay = req.body.relay
         const accountDetail = req.body.accountDetail;
         const payloadJson = {
             "sourceAsset": sourceAsset,
-            "sourceAmount":amount,
-            "targetNetwork":targetNetwork,
-            "targetAsset":targetAsset,
-            "targetAccount": accountDetail ? accountDetail : {
-                "account":krackenWalletId, // our Kraken account ID for that asset.
-                // DANGER!!!!! This must be the exact wallet ID per asset in question.  It will
-                // be different for each asset, etc:  BTC, XLM, ETH...
-                "memo":memo,
-                "memoType":"text"
-            },
-            //"relay":"BTC:GBVOL67TMUQBGL4TZYNMY3ZQ5WGQYFPFD5VJRWXR72VA33VFNL225PL5"
+            "sourceAmount": amount,
+            "targetNetwork": targetNetwork,
+            ...(targetAsset && {
+                "targetAsset": targetAsset
+            }),
+            targetAccount: accountDetail,
+            ...(relay && { "relay": relay })
         }
+        console.log('payload', pa)
         const withdrawalResponse = await btcRequest('withdrawal', payloadJson, 'post');
         if (withdrawalResponse.status !== 200) {
             // something went wrong, let's abort and debug by looking at our log file
