34db882c73dfa9f6e9ef978267d41a49a2ff361d
Dawit Amanuel <ahizkias@gmail.com>
2021-10-05 16:16:31 +0300

    [improve] Add support for all asset requests. No validation yet

--- coinqvest_withdrawal/index.js
+++ coinqvest_withdrawal/index.js
@@ -9,37 +9,33 @@ module.exports = async function (context, req) {
         const amount = req.body.amount;
         const targetNetwork = req.body.targetNetwork;
         const targetAsset = req.body.targetAsset;
-        const memo = req.body.memo;
-        const krackenWalletId = req.body.krackenWalletId;
         const sourceAsset = req.body.sourceAsset;
+        const relay = req.body.relay
         const accountDetail = req.body.accountDetail;
         const payloadJson = {
             "sourceAsset": sourceAsset,
-            "sourceAmount":amount,
-            "targetNetwork":targetNetwork,
-            "targetAsset":targetAsset,
-            "targetAccount": accountDetail ? accountDetail : {
-                "account":krackenWalletId, // our Kraken account ID for that asset.
-                // DANGER!!!!! This must be the exact wallet ID per asset in question.  It will
-                // be different for each asset, etc:  BTC, XLM, ETH...
-                "memo":memo,
-                "memoType":"text"
-            },
-            //"relay":"BTC:GBVOL67TMUQBGL4TZYNMY3ZQ5WGQYFPFD5VJRWXR72VA33VFNL225PL5"
+            "sourceAmount": amount,
+            "targetNetwork": targetNetwork,
+            ...(targetAsset && {
+                "targetAsset": targetAsset
+            }),
+            targetAccount: accountDetail,
+            ...(relay && { "relay": relay })
         }
+        console.log('payload', pa)
         const withdrawalResponse = await btcRequest('withdrawal', payloadJson, 'post');
         if (withdrawalResponse.status !== 200) {
             // something went wrong, let's abort and debug by looking at our log file
             console.log('Could not initiate withdrawal.');
             const res = {
                 status: 500,
-                body: {"status": "error", "msg": "coult not initiate withdrawal"}
+                body: { "status": "error", "msg": "coult not initiate withdrawal" }
             }
             handleResponse(context, res);
         }
         const withdrawalId = withdrawalResponse.data.withdrawal.id; // store this persistently in your 
-        const commitPayloadJson ={
-            "withdrawalId":withdrawalId
+        const commitPayloadJson = {
+            "withdrawalId": withdrawalId
         }
         const result = await btcRequest('withdrawal/commit', commitPayloadJson, 'post');
         if (result.status !== 200) {
@@ -47,14 +43,14 @@ module.exports = async function (context, req) {
             console.log('Could not finish withdrawal.', withdrawalId);
             const res = {
                 status: 500,
-                body: {"status": "error", "msg": "could not finish withdrawal"}
+                body: { "status": "error", "msg": "could not finish withdrawal" }
             }
             handleResponse(context, res);
         }
         else {
             context.res = {
                 status: 200,
-                body: {response}
+                body: { response }
             }
             handleResponse(context, res);
         }
